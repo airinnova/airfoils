d5147a8e3ce970f57545f8233aac3a314a6f98de
Aaron Dettmann <dettmann@kth.se>
2020-01-08 19:33:46 +0100

    Update year in licence header

--- src/airfoils/fileio.py
+++ src/airfoils/fileio.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # ----------------------------------------------------------------------
-# Copyright 2017-2019 Airinnova AB and the Airfoils authors
+# Copyright 2017-2020 Airinnova AB and the Airfoils authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
